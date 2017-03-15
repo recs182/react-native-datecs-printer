82b767d76984433352c2f80731f8543d59115c68
Renan Soares <renan-csoares@hotmail.com>
2017-03-15 08:02:57 -0300

    tests in real app to see if it's working

--- index.js
+++ index.js
@@ -4,8 +4,3 @@ import { NativeModules } from 'react-native';
 const RNDatecsPrinter = NativeModules.RNDatecsPrinter;
 
 module.exports = RNDatecsPrinter;
-
-// export default {
-// 	printTaggedText: RNDatecsPrinter.printTaggedText(),
-// 	printSelfTest: RNDatecsPrinter.printSelfTest()
-// };
